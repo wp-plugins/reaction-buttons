875ae815a1d297db14c07fa567abcc7aeadc3f4f
jakob42 <jakob42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-01 08:35:19 +0000

    wrong version in reaction_buttons.php
    git-svn-id: https://plugins.svn.wordpress.org/reaction-buttons/tags/0.9.6@223932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reaction_buttons.php
+++ reaction_buttons.php
@@ -3,7 +3,7 @@
    Plugin Name: Reaction Buttons
    Plugin URI: http://blog.jl42.de/reaction-buttons/
    Description: Adds Buttons for very simple and fast feedback to your post. Inspired by Blogger.
-   Version: 0.9.5
+   Version: 0.9.6
    Author: Jakob Lenfers
    Author URI: http://blog.jl42.de
 
